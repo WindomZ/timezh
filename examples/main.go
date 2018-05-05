8f8f8084dd2483292eed19b1923a97f5de29e486
WindomZ <git.windomz@gmail.com>
2018-05-05 19:02:38 +0800

    add format example

---
+++ examples/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/WindomZ/timezh"
+)
+
+func main() {
+	t, _ := time.Parse("2006-01-02 15:04:05", "2009-01-03 18:15:05")
+
+	fmt.Println("Time:", t.Format("2006-01-02 15:04:05"))
+
+	format(t)
+}
+
+func format(t time.Time) {
+	fmt.Println("===========Format===========")
+	fmt.Println("\"time\"效果：")
+	fmt.Println("  - time.Format(英文):  ", t.Format("2006-01-02(January) PM3:04:05 Monday(Mon)"))
+	fmt.Println("  - time.Format(中文):  ", t.Format("2006年01月02日(January) PM3:04:05 Monday(Mon)"))
+
+	fmt.Println("\"timezh\"同样可以：")
+	fmt.Println("  - timezh.Format(英文):", timezh.T(t).Format("2006-01-02(January) PM3:04:05 Monday(Mon)"))
+	fmt.Println("  - timezh.Format(中文):", timezh.T(t).Format("2006年01月02日(January) PM3:04:05 Monday(Mon)"))
+
+	fmt.Println("\"timezh\"还可以：")
+	fmt.Println("  - timezh.Format(特性):", timezh.T(t).Format("2006年01月02日(一月) 下午3:04:05 星期一(周一)"))
+	fmt.Println("  - timezh.Format(特性):", timezh.T(t).Format("2006年01月02日(January, 一月) 下午3:04:05PM 星期一(Mon, 周一)"))
+}
